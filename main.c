b566889381007d933116283d0814cd63c0a393d1
William R <williamucep@hotmail.com>
2019-05-22 21:26:51 -0300

    Finaliza o menu

--- main.c
+++ main.c
@@ -2,38 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define SAIR 7
+
+void mostraMenu();
+void pegaOpcaoUsuario(int *opcao);
+
 int main() {
 
+	char nome_imagem[50];
+	FILE *arquivo_imagem;
 	int opcao = 0;
 
-	char nome_imagem[50];
-	FILE *ArquivoImagem;
-
-
+	// Pega o nome da imagem é adiciona a extensão ".ppm"
 	printf(">>> Informe o nome da imagem: ");
 	scanf("%s", nome_imagem);
-
 	strcat(nome_imagem, ".ppm");
 
-	ArquivoImagem = fopen(nome_imagem, "r+");
-
-	if (ArquivoImagem == NULL){
-		printf("Imagem nao carregada");	
+	// Faz a leitura da imagem baseada no nome
+	arquivo_imagem = fopen(nome_imagem, "r+");
+	if (arquivo_imagem == NULL){
+		printf("A imagem informada não existe.\n");
 		return 1;
+
 	} else {
+
 		do {
+			// Pega a opção do usuário passando o endereço
+			// da variável que vai ser armazenada a opção
+			// + Nome da variável: opcao
+			pegaOpcaoUsuario(&opcao);
 
-		printf("1 - para binarização usando thresholding\n");
-		printf("2 - executa blurring\n");
-		printf("3 - executa sharpening\n");
-		printf("4 - rotação da imagem\n");
-		printf("5 - ampliar a imagem\n");
-		printf("6 - reduzir a imagem\n");
-		printf("7 - sair\n");
-		scanf("%d", &opcao);
-
-	} while(opcao != 7);
-	}	
+		} while(opcao != SAIR);
+	}
 
 	return 0;
+}
+
+void pegaOpcaoUsuario(int *opcao) {
+	mostraMenu();
+	scanf("%d", opcao);
+}
+
+void mostraMenu() {
+	printf("1 - Binarização usando thresholding\n");
+	printf("2 - Blurring\n");
+	printf("3 - Executa sharpening\n");
+	printf("4 - Rotação da imagem\n");
+	printf("5 - Ampliar a imagem\n");
+	printf("6 - Reduzir a imagem\n");
+	printf("7 - Sair\n");
 }
